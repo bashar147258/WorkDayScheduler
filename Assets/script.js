1a8a0a8b2cc0bf85dda6909d79244ee96ced5871
bashar147258 <89011274+bashar147258@users.noreply.github.com>
2021-09-23 21:20:55 +1000

    Fixed Local Storage

--- Assets/script.js
+++ Assets/script.js
@@ -3,15 +3,15 @@ var currDay = $("#currentDay").text(moment().format("dddd, MMMM Do, h:mm A"));
 
 //Get the stored todos from the time blocks
 function showToDos(){
-  $("#9").val(localStorage.getItem("9"));
-  $("#10").val(localStorage.getItem("10"));
-  $("#11").val(localStorage.getItem("11"));
-  $("#12").val(localStorage.getItem("12"));
-  $("#13").val(localStorage.getItem("13"));
-  $("#14").val(localStorage.getItem("14"));
-  $("#15").val(localStorage.getItem("15"));
-  $("#16").val(localStorage.getItem("16"));
-  $("#17").val(localStorage.getItem("17"));
+  $("#9 textarea").val(localStorage.getItem("9"));
+  $("#10 textarea").val(localStorage.getItem("10"));
+  $("#11 textarea").val(localStorage.getItem("11"));
+  $("#12 textarea").val(localStorage.getItem("12"));
+  $("#13 textarea").val(localStorage.getItem("13"));
+  $("#14 textarea").val(localStorage.getItem("14"));
+  $("#15 textarea").val(localStorage.getItem("15"));
+  $("#16 textarea").val(localStorage.getItem("16"));
+  $("#17 textarea").val(localStorage.getItem("17"));
 }
 
 //Save Button
